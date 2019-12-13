15b5133a7acd2c863320201f60a102af0fe2075d
wangchengjin <tutor66@gmail.com>
2019-12-13 10:23:16 +0800

    first commit

---
+++ src/main/java/com/zyc/summer/springboot/mybatisplus/SummerApplication.java
@@ -0,0 +1,13 @@
+package com.zyc.summer.springboot.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyc/summer/springboot/mybatisplus/SummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyc.summer.springboot.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
